4f1fc68a6ec74b6bd9773c8b4c5315b9e91cd646
03662232677 <03662232677@serpro-1540215>
2017-02-07 10:32:38 -0200

    teste

--- app.js
+++ app.js
@@ -15,14 +15,48 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+console.log('abriu');
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+app.get('/resultado',function(req,resp){
+	console.log('resultado');
+	var Crawler = require("crawler");
+	var url = require('url');
+ 
+	var c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page 
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default 
+            //a lean implementation of core jQuery designed specifically for the server 
+            console.log($);
+			var json = JSON.stringify({ 
+				title: $("title").text(), 
+				html: $("html").text()
+			});
+			resp.contentType('application/json');
+			resp.send(json);
+			
+        }
+        done();
+    }
+});
+c.queue('http://www.amazon.com');
+});
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+
